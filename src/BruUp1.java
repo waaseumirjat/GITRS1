840921be748aa6e64a632dc6682762029ead4283
Waaseu <muhammadwaaseu.mirjat@cgi.com>
2023-10-18 17:04:19 -0400

    First commit for Java Brush up

---
+++ src/BruUp1.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BruUp1 {
+	
+	
+	
+	
+	public static void main(String[] args) {
+		int[] arr = new int[5];
+		arr[0] = 7;
+		int[] arr2 = {1, 2, 3, 4, 5};
+		System.out.println(arr2[1]);
+		
+		for (int i= 0; i < arr2.length; i++) {
+			System.out.println(arr2[i]);
+		}
+		
+		String[] arr3 = {"waaseu", "mirjat", "selenium"};
+		for (int j= 0; j < arr3.length; j++) {
+			System.out.println(arr3[j]);
+		}
+		
+		
+		for (String s:arr3){
+			System.out.println(s);
+			}
+
+	}
+	
+	
+	
+
+}
---
+++ src/BruUp2.java
@@ -0,0 +1,26 @@
+
+public class BruUp2 {
+
+	public static void main(String[] args) {
+		
+		int[] arr1 = {1, 2, 3, 4, 5, 6, 8, 9, 122};
+		
+		for (int i = 0; i<arr1.length; i++) {
+			if (arr1[i] % 2 == 0) {
+				
+				
+				System.out.println(arr1[i]);
+				
+			}
+			else {
+				System.out.println(arr1[i] + "is not a multiple of 2");
+			}
+			
+			
+			
+		}
+		
+
+	}
+
+}
