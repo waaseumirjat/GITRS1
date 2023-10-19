5a63275c644a2a3115841d762eb13588910b344b
Waaseu <muhammadwaaseu.mirjat@cgi.com>
2023-10-19 15:48:34 -0400

    Main guy commit

--- src/BruUp2.java
+++ src/BruUp2.java
@@ -8,9 +8,7 @@ public class BruUp2 {
 		for (int i = 0; i<arr1.length; i++) {
 			if (arr1[i] % 2 == 0) {
 				
-				
 				System.out.println(arr1[i]);
-				
 			}
 			else {
 				System.out.println(arr1[i] + "is not a multiple of 2");
@@ -18,8 +16,11 @@ public class BruUp2 {
 			
 		}
 		System.out.println("We are doing full training");
-		String[] arr2 = {"Waaseu", "Sameer"};
+		String[] arr2 = {"Waaseu", "Sameer", "Medhat"};
 		System.out.println(arr2[1]);
+		for (String s:arr2) {
+			System.out.println(s);
+		}
 	}
 
 }
