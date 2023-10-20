cfc98141428c76a54eb6d62f063ac9675e932242
Waaseu <muhammadwaaseu.mirjat@cgi.com>
2023-10-20 15:12:52 -0400

    Architect in Asia

--- src/BruUp2.java
+++ src/BruUp2.java
@@ -3,7 +3,7 @@ public class BruUp2 {
 
 	public static void main(String[] args) {
 		
-		int[] arr1 = {1, 2, 3, 4, 5, 6, 8, 9, 122};
+		int[] arr1 = {1, 2, 3, 4, 5, 6, 8, 9};
 		
 		for (int i = 0; i<arr1.length; i++) {
 			if (arr1[i] % 2 == 0) {
@@ -22,6 +22,8 @@ public class BruUp2 {
 			System.out.println(s);
 		}
 		System.out.println("we are progressing to final phase of GIT training");
+		System.out.println("Now we doing branching training 1");
+		System.out.println("Now we doing branching training 2");
 	}
 
 }
